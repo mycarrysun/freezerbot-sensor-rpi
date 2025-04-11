7e2f1a5abaf13f7e935e49d6ce6b36e8f600b3a8
Mike Harrison <mike@nextwebtoday.com>
2025-04-11 09:03:58 -0400

    Only use tcp for pisugar lib

--- raspberry_pi/battery.py
+++ raspberry_pi/battery.py
@@ -1,6 +1,5 @@
-import socket
 import traceback
-from pisugar import PiSugarServer, connect_tcp, connect_domain_socket, test_via_tcp
+from pisugar import PiSugarServer, connect_tcp, test_via_tcp
 
 
 class PiSugarMonitor:
@@ -9,43 +8,54 @@ class PiSugarMonitor:
     def __init__(self):
         """Initialize the PiSugar monitoring capabilities through the official library"""
         try:
-            # First try domain socket connection, which is the default
-            conn, event_conn = connect_domain_socket()
+            conn, event_conn = connect_tcp()
             self.server = PiSugarServer(conn, event_conn)
-            print("PiSugar connected via domain socket")
-        except Exception:
-            # Fall back to TCP if domain socket fails
-            try:
-                conn, event_conn = connect_tcp()
-                self.server = PiSugarServer(conn, event_conn)
-                print("PiSugar connected via TCP")
-            except Exception as e:
-                print(f"Failed to connect to PiSugar: {traceback.format_exc()}")
-                self.server = None
+            print("PiSugar connected via TCP")
+        except Exception as e:
+            print(f"Failed to connect to PiSugar: {traceback.format_exc()}")
+            self.server = None
 
     def get_battery_level(self):
         if self.server is not None:
-            return self.server.get_battery_level()
+            try:
+                return self.server.get_battery_level()
+            except:
+                pass
 
     def get_current(self):
         if self.server is not None:
-            return self.server.get_battery_current()
+            try:
+                return self.server.get_battery_current()
+            except:
+                pass
 
     def get_voltage(self):
         if self.server is not None:
-            return self.server.get_battery_voltage()
+            try:
+                return self.server.get_battery_voltage()
+            except:
+                pass
 
     def is_charging(self):
         if self.server is not None:
-            return self.server.get_battery_charging()
+            try:
+                return self.server.get_battery_charging()
+            except:
+                pass
 
     def is_power_plugged(self):
         if self.server is not None:
-            return self.server.get_battery_power_plugged()
+            try:
+                return self.server.get_battery_power_plugged()
+            except:
+                pass
 
     def is_charging_allowed(self):
         if self.server is not None:
-            return self.server.get_battery_allow_charging()
+            try:
+                return self.server.get_battery_allow_charging()
+            except:
+                pass
 
     # TODO add setter for battery charging range
     # https://github.com/PiSugar/pisugar-server-py/blob/main/pisugar/pisugar.py#L287
