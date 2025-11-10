53cb6d6b53a802729cee07f81815cb83243fd68d
Mike Harrison <mike@nextwebtoday.com>
2025-11-09 22:02:29 -0500

    Remove sudo calls from install-system-deps script

--- install-system-deps.sh
+++ install-system-deps.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # This file is only used in firmware_updater.py to install new system dependencies that were added after initial launch
+# Note: This script is run with sudo by firmware_updater.py, so we don't need sudo commands here
 
-sudo apt-get update
+set -e  # Exit on any error
+set -u  # Exit on undefined variables
+
+apt-get update
 # install some libs for the i2c oled screen
-sudo apt-get install -y \
+apt-get install -y \
   libopenjp2-7 \
   libtiff6 \
   libatlas-base-dev
